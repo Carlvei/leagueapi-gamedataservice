a6f414e56e66d330a6ac353066086f6ccca08e92
carlo.spinello <carlo.spinello@adesso.at>
2023-03-02 10:39:39 +0100

    feat: project setup

---
+++ src/main/java/at/adesso/leagueapi/gamedataservice/GamedataserviceApplication.java
@@ -0,0 +1,13 @@
+package at.adesso.leagueapi.gamedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedataserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamedataserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/adesso/leagueapi/gamedataservice/GamedataserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.adesso.leagueapi.gamedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamedataserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
